35a4356d22b5a457069e4186b3730be98734a7fe
fox <fernandodiego.sn@gmail.com>
2023-08-06 17:40:52 -0300

    Link estilizado

--- nextjs-alura/pages/faq.js
+++ nextjs-alura/pages/faq.js
@@ -3,7 +3,7 @@ import Link from "../src/components/Link";
 export default function FAQPage() {
     return (
         <div>
-            <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
+            <h1>Alura Cases - Páginas de Perguntas Frequentes - FAQ</h1>
             <Link href="/">Ir para a home</Link>
         </div>
     )
