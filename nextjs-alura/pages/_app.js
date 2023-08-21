2b2bd0a8a6bf51f1df087c46920789c626fc93f6
fox <fernandodiego.sn@gmail.com>
2023-08-21 14:47:38 -0300

    head, titulo da aba, fontes precarregadas

--- nextjs-alura/pages/_app.js
+++ nextjs-alura/pages/_app.js
@@ -1,9 +1,16 @@
 import GlobalStyle from '../src/theme/GlobalStyle';
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
     return (
         <>
-            <GlobalStyle />
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap"
+                      rel="stylesheet"/>
+            </Head>
+            <GlobalStyle/>
             <Component {...pageProps} />
         </>
     )
